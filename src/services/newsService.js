7125d2c23268b2d8aefe97a9145d02868b0906c3
Nuno Pacheco <nunorpacheco@gmail.com>
2020-11-14 09:46:21 +0000

    fix

--- src/services/newsService.js
+++ src/services/newsService.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 
 
 const service = axios.create({
-    baseURL: 'https://newsapi.org/v2',
+    baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 
 
 export const getCovidNews = async () => {
     try {
-        const coronaNews = await service.get(`/everything?q=Covid&apiKey=${process.env.REACT_APP_API_KEY}`);
+        const coronaNews = await service.get(`/news`);
         return coronaNews.data;
     } catch (error) {
         return error;
